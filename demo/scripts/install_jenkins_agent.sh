e96c836ef0dfaa448003ecb4edbb93581ed79e97
Yonathan Koren <yonkornilov@live.com>
2019-09-07 23:26:06 -0400

    Update gitignore to use entire jenkinsfile-runner target dir; add bootstrap_jenkins, run_jenkins scripts; forward port for jenkins installation; install Java on agent.

--- demo/scripts/install_jenkins_agent.sh
+++ demo/scripts/install_jenkins_agent.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 apt-get update
-apt-get install unzip docker -y
+apt-get install unzip docker default-jre -y
 wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war -O /tmp/jenkins.war
 unzip /tmp/jenkins.war -d /tmp/jenkins
