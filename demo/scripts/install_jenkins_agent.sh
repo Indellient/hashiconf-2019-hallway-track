c15b956c3bbd53b9070a4fb12009dd86ee9ddcb3
Yonathan Koren <yonkornilov@live.com>
2019-09-09 08:53:26 -0700

    Intall Habitat on Jenkins.

--- demo/scripts/install_jenkins_agent.sh
+++ demo/scripts/install_jenkins_agent.sh
@@ -4,3 +4,5 @@ apt-get update
 apt-get install unzip docker default-jre -y
 wget http://mirrors.jenkins.io/war-stable/latest/jenkins.war -O /tmp/jenkins.war
 unzip /tmp/jenkins.war -d /tmp/jenkins
+
+curl https://raw.githubusercontent.com/habitat-sh/habitat/master/components/hab/install.sh | sudo bash
